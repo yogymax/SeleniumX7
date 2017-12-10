c8093899db4818f211a195bf50e3e641befaefd3
yogymax@gmail.com <yogymax@gmail.com>
2017-12-10 10:43:10 +0530

    Local Commit

--- src/test/java/com/scp/selenium/seleniumBatchSeven/DesktopAutomation.java
+++ src/test/java/com/scp/selenium/seleniumBatchSeven/DesktopAutomation.java
@@ -4,6 +4,28 @@ public class DesktopAutomation {
 
 	
 	public static void main(String[] args) {
-		
+		/**
+		 * 
+		 * Commit
+		 * Commit and Push
+		 * 
+		 * Revision
+		 * Head
+		 * 
+		 * Synchronize
+		 * 
+		 * Compare With
+		 * 		Revision
+		 *      Head
+		 *      
+		 * Replace With
+		 * 		Revision
+		 *      Head
+		 *      
+		 * Pull
+		 * 
+		 * 
+		 * 
+		 */
 	}
 }
