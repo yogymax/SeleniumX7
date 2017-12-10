24067a39d45108bab833a9932249750c57f504fd
yogymax@gmail.com <yogymax@gmail.com>
2017-12-10 10:44:39 +0530

    Commit

--- src/main/java/com/scp/selenium/seleniumBatchSeven/App.java
+++ src/main/java/com/scp/selenium/seleniumBatchSeven/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("added new comment");
+        System.out.println("AAAdded new comment");
     }
 }
